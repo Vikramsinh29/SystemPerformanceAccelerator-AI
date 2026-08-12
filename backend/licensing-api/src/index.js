4c664aad5f4966452446845fd88988b09dc47585
Vikramsinh29 <vikramsinh29@gmail.com>
2026-08-12 11:19:36 +0530

    chore: export production identity verifier

--- backend/licensing-api/src/index.js
+++ backend/licensing-api/src/index.js
@@ -6,6 +6,7 @@ export { createLicensingComposition } from "./licensing-composition.js";
 export { LicensingIdentityError, createLicensingIdentityBridge } from "./licensing-identity-bridge.js";
 export { LICENSING_RUNTIME_OPERATIONS, createLicensingWorkerRuntime } from "./licensing-worker-runtime.js";
 export { createLicensingStagingRouter } from "./licensing-staging-router.js";
+export { ProductionIdentityError, createProductionIdentityResolver } from "./production-identity-verifier.js";
 
 export default {
   async fetch() {
